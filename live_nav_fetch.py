feb190b31647c7d684e68890931e462353a79083
Aitha Ramakrishna <ramakrishna@example.com>
2026-06-02 11:42:55 +0530

    Day 1: Data ingestion complete

---
+++ live_nav_fetch.py
@@ -0,0 +1,33 @@
+import requests
+import pandas as pd
+
+funds = {
+    "HDFC_Top100": 125497,
+    "SBI_Bluechip": 119551,
+    "ICICI_Bluechip": 120503,
+    "Nippon_LargeCap": 118632,
+    "Axis_Bluechip": 119092,
+    "Kotak_Bluechip": 120841
+}
+
+for fund_name, code in funds.items():
+
+    url = f"https://api.mfapi.in/mf/{code}"
+
+    response = requests.get(url)
+
+    if response.status_code == 200:
+
+        data = response.json()
+
+        df = pd.DataFrame(data["data"])
+
+        df.to_csv(
+            f"data/raw/{fund_name}.csv",
+            index=False
+        )
+
+        print(f"{fund_name} downloaded successfully")
+
+    else:
+        print(f"Failed to download {fund_name}")
